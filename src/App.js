d535a5c9a3a15d69a03830d682a0fafd46ad5bf9
Tim Rüdenauer <timrudenauer@gmail.com>
2020-07-19 16:18:37 +0200

    Add "Fork me on GitHub" ribbon

--- src/App.js
+++ src/App.js
@@ -363,6 +363,20 @@ class App extends Component {
               />
             )}
           </aside>
+          <a
+            id="forkMe"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/tim-the-arcane/webstrukto">
+            <img
+              width="149"
+              height="149"
+              src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149"
+              class="attachment-full size-full"
+              alt="Fork me on GitHub"
+              data-recalc-dims="1"
+            />
+          </a>
         </div>
       </div>
     );
