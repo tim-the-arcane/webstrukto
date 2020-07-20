9f6c734b799830dd278c78d875cff16101ed712b
Tim Rüdenauer <timrudenauer@gmail.com>
2020-07-20 23:42:55 +0200

    Add SymbolPlaceholder component

---
+++ src/components/Symbol/_SymbolPlaceholder.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const _SymbolPlaceholder = () => {
+  return (
+    <div className="Symbol Symbol--Placeholder">
+      <div className="Symbol__header">
+        <span className="Symbol__title">Hier landet das neue Symbol</span>
+      </div>
+    </div>
+  );
+};
+
+export default _SymbolPlaceholder;
