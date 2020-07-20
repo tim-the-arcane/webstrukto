4dbe7c8d6d226711e542a3abbbff0d3edd7385ba
Tim Rüdenauer <timrudenauer@gmail.com>
2020-07-21 00:44:35 +0200

    Create blank Symbol component

---
+++ src/components/Symbol/_SymbolBlank.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const _SymbolBlank = () => {
+  return (
+    <div className="Symbol Symbol--blank">
+      <div className="Symbol__header">
+        <span className="Symbol__title">
+          Hier kannst du ein Symbol einfügen...
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default _SymbolBlank;
