880d04b1c9f8d1931994d59bbef29721a05f7bf5
Tim Rüdenauer <timrudenauer@gmail.com>
2020-07-20 22:33:10 +0200

    Fix button popup hints in SymbolButtons

--- src/components/Symbol/_SymbolButtons.js
+++ src/components/Symbol/_SymbolButtons.js
@@ -20,41 +20,42 @@ const SymbolButtons = ({
       {addSymbol && (
         <button
           className="Symbol__button Symbol__button--add-child"
-          onClick={() => addSymbol(symbol.id)}>
-          <span
-            role="img"
-            aria-label={`Sub-Symbol zu '${symbol.title} hinzufügen'`}>
+          onClick={() => addSymbol(symbol.id)}
+          title={`Sub-Symbol zu '${symbol.title} hinzufügen'`}>
+          <span role="img">
             <AddTwoTone />
           </span>
         </button>
       )}
       <button
         className="Symbol__button Symbol__button--move-up"
-        onClick={() => moveSymbol(symbol.id, "UP")}>
-        <span role="img" aria-label={`move Symbol titled '${symbol.title}' up`}>
+        onClick={() => moveSymbol(symbol.id, "UP")}
+        title={`Bewege '${symbol.title}' nach oben`}>
+        <span role="img">
           <ArrowUpwardTwoTone />
         </span>
       </button>
       <button
         className="Symbol__button Symbol__button--move-down"
-        onClick={() => moveSymbol(symbol.id, "DOWN")}>
-        <span
-          role="img"
-          aria-label={`move Symbol titled '${symbol.title}' down`}>
+        onClick={() => moveSymbol(symbol.id, "DOWN")}
+        title={`Bewege '${symbol.title}' nach unten`}>
+        <span role="img">
           <ArrowDownwardTwoTone />
         </span>
       </button>
       <button
         className="Symbol__button Symbol__button--edit"
-        onClick={() => editSymbol(symbol)}>
-        <span role="img" aria-label={`remove Symbol titled '${symbol.title}'`}>
+        onClick={() => editSymbol(symbol)}
+        title={`Bearbeite '${symbol.title}'`}>
+        <span role="img">
           <EditTwoTone />
         </span>
       </button>
       <button
         className="Symbol__button Symbol__button--delete"
-        onClick={() => removeSymbol(symbol.id)}>
-        <span role="img" aria-label={`remove Symbol titled '${symbol.title}'`}>
+        onClick={() => removeSymbol(symbol.id)}
+        title={`Lösche '${symbol.title}'`}>
+        <span role="img">
           <DeleteForeverTwoTone />
         </span>
       </button>
