f68b6e9eda6efe243e5d783b4e24e8078eb93573
Tim Rüdenauer <timrudenauer@gmail.com>
2020-07-21 15:54:16 +0200

    Add onClick event for blank symbol to add a new child

--- src/components/Symbol/_SymbolBlank.js
+++ src/components/Symbol/_SymbolBlank.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-const _SymbolBlank = () => {
+const _SymbolBlank = props => {
   return (
-    <div className="Symbol Symbol--blank">
+    <div
+      className="Symbol Symbol--blank"
+      onClick={() => props.addSymbol(props.parentSymbolId)}>
       <div className="Symbol__header">
         <span className="Symbol__title">
           Hier kannst du ein Symbol einfügen...
